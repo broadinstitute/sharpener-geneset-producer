a0978f7ff1163b467c2f9a1671ae262075ae6d37
vdancik <vdancik@broadinstitute.org>
2019-08-15 17:11:24 -0400

    implement gene-transformer API version 1.1

--- java-play-framework-server/app/producer/Producer.java
+++ java-play-framework-server/app/producer/Producer.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import apimodels.Attribute;
 import apimodels.GeneInfo;
+import apimodels.GeneInfoIdentifiers;
 import apimodels.Parameter;
 import apimodels.Property;
 import apimodels.TransformerInfo;
@@ -16,6 +17,10 @@ import apimodels.TransformerQuery;
 
 public class Producer {
 
+	private static final TransformerInfo.FunctionEnum PRODUCER = TransformerInfo.FunctionEnum.PRODUCER;
+	
+	private static final Parameter.TypeEnum STRING = Parameter.TypeEnum.STRING;
+	
 	private static final String GENE_SET_NAME = "gene_set_name";
 
 	private static HashMap<String,ArrayList<GeneInfo>> geneSets = new HashMap<String,ArrayList<GeneInfo>>();
@@ -30,8 +35,10 @@ public class Producer {
 
 
 	public static TransformerInfo transformerInfo() {
-		TransformerInfo transformerInfo = new TransformerInfo().name("MSigDB gene-set producer").function("producer");
-		transformerInfo.addParametersItem(new Parameter().name(GENE_SET_NAME).type("string"));
+		TransformerInfo transformerInfo = new TransformerInfo().name("MSigDB gene-set producer").function(PRODUCER);
+		transformerInfo.description("Gene-list producer based on MSigDB gene sets (http://software.broadinstitute.org/gsea/msigdb/index.jsp).");
+		transformerInfo.addParametersItem(new Parameter().name(GENE_SET_NAME).type(STRING)._default("REACTOME_GLYCOLYSIS"));
+		transformerInfo.setRequiredAttributes(new ArrayList<String>());
 		return transformerInfo;
 	}
 
@@ -42,9 +49,11 @@ public class Producer {
 			String[] row = line.split("\t");
 			ArrayList<GeneInfo> genes = new ArrayList<GeneInfo>();
 			for (int i = 2; i < row.length; i++) {
-				GeneInfo gene = new GeneInfo().geneId("NCBIgene:" + row[i]);
+				String geneId = "NCBIgene:" + row[i];
+				GeneInfo gene = new GeneInfo().geneId(geneId);
 				gene.addAttributesItem(new Attribute().name("entrez_gene_id").value(row[i]).source("MSigDB"));
 				gene.addAttributesItem(new Attribute().name("gene set").value(row[0]).source("MSigDB"));
+				gene.setIdentifiers(new GeneInfoIdentifiers().entrez(geneId));
 				genes.add(gene);
 			}
 			geneSets.put(row[0], genes);
